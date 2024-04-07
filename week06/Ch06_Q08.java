1d6d2f11791d10fd55831c72ffcff8369030f28b
lshbluesky <61459016+lshbluesky@users.noreply.github.com>
2024-04-07 22:13:27 +0900

    [Java] 5장 연습문제 8번

---
+++ week06/Ch06_Q08.java
@@ -0,0 +1,15 @@
+package week06;
+
+public class Ch06_Q08 {
+	public static void main(String[] args) {
+		String str = "내 이름은 [홍길동]입니다. 나이는 [15]살 입니다.";
+		String name;
+		int age=0;
+		
+		name = str.substring(str.indexOf('[')+1, str.indexOf(']'));
+		age = Integer.parseInt(str.substring(str.lastIndexOf('[')+1, str.lastIndexOf(']')));
+		
+		System.out.println(name);
+		System.out.println(age);
+	}
+}
