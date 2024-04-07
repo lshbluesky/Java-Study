74dc12637c3765fcf764cc321a25c17427d746d3
lshbluesky <61459016+lshbluesky@users.noreply.github.com>
2024-04-07 22:12:40 +0900

    [기능 개선] LottoNumber.java - 로또 번호 중복 제거

--- week05/LottoNumber.java
+++ week05/LottoNumber.java
@@ -6,18 +6,27 @@ public class LottoNumber {
 	public static void main(String[] args) {
 		Random rand = new Random();
 		final int LOTTO_NUM = 6;
-		final int MAX_NUM = 10;
+		final int MAX_NUM = 6;
 
 		int[] a = new int[LOTTO_NUM];
 
 		for (int i = 0; i < LOTTO_NUM; i++) {
-			if (i == 0)
-				a[i] = rand.nextInt(MAX_NUM) + 1;
-			else {
-				a[i] = rand.nextInt(MAX_NUM) + 1;
-				if (a[i] == a[i - 1])
-					a[i] = rand.nextInt(MAX_NUM) + 1;
-			}
+			boolean isDuplicate = false;
+			int randNum;
+
+			do {
+				isDuplicate = false;
+				randNum = rand.nextInt(MAX_NUM) + 1;
+
+				for (int j = 0; j < i; j++) { // 중복 검사
+					if (a[j] == randNum) {
+						isDuplicate = true;
+						break;
+					}
+				}
+			} while (isDuplicate);
+
+			a[i] = randNum;
 		}
 
 		for (int i = 0; i < LOTTO_NUM; i++) {
