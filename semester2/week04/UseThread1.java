d536900245537e9f2f46abd4fcb63bc648e5017b
lshbluesky <61459016+lshbluesky@users.noreply.github.com>
2024-10-01 17:56:15 +0900

    [Fix] UseThread1.java - 클래스 이름 수정

--- semester2/week04/UseThread1.java
+++ semester2/week04/UseThread1.java
@@ -1,7 +1,7 @@
 package week04;
 
-// ScriptThread : 자막을 송출하는 기능이 있는 run() 메소드 재정의.
-class ScriptThread extends Thread {
+// CaptionThread : 자막을 송출하는 기능이 있는 run() 메소드 재정의.
+class CaptionThread extends Thread {
 	@Override
 	public void run() {
 		String[] script = { "하나", "둘", "셋", "넷", "다섯" };
@@ -24,8 +24,8 @@ class ScriptThread extends Thread {
 // main 스레드 : 비디오 프레임(장면)을 송출하는 기능
 public class UseThread1 {
 	public static void main(String[] args) {
-		ScriptThread script = new ScriptThread();
-		script.start();
+		CaptionThread caption = new CaptionThread();
+		caption.start();
 
 		int[] scene = { 1, 2, 3, 4, 5 };
 		for (int num : scene) {
